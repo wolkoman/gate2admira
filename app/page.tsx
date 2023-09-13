51011ffe8b0a89dff0d40784ef6c4c50cf70bca2
Manuel Wolkowitsch <superwolko@gmail.com>
2023-09-13 10:21:57 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import {contentfulDocumentEntry} from "@/app/(contentful)/fetch";
+import {ContentfulDocumentRenderer} from "@/app/(contentful)/renderer";
+
 export default async function Home() {
 
-  return <main className="">
-    <div className="flex relative">
-
-    </div>
-
-  </main>
+  const entry = await contentfulDocumentEntry("6XdxqW4vh930hK4G3MOyRA");
+  return <>
+    <div className="text-4xl font-bold mb-12">{entry.fields.title}</div>
+    <ContentfulDocumentRenderer document={entry.fields.content}/>
+  </>
 }
